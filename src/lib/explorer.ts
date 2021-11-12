9b9c402a213686acbbdbdb8971de2af39a03684d
TheBoringDude <iamcoderx@gmail.com>
2021-11-12 19:04:03 +0800

    remove some unused and throw errors on needed variables

--- src/lib/explorer.ts
+++
@@ -1,7 +0,0 @@
-import { ExplorerApi } from 'atomicassets';
-
-const explorer = new ExplorerApi(process.env.NEXT_PUBLIC_ATOMICASSETS_API, 'atomicassets', {
-  fetch
-});
-
-export { explorer };
--- src/lib/waxnet.ts
+++ src/lib/waxnet.ts
@@ -2,4 +2,15 @@ const chainId = process.env.NEXT_PUBLIC_WAXCHAIN ?? '';
 const endpoint = process.env.NEXT_PUBLIC_WAXNET ?? '';
 const dapp = process.env.NEXT_PUBLIC_CONTRACTNAME ?? '';
 
+// throw errors if they are blank / empty
+if (!chainId) {
+  throw new Error('No ChainID set in environment variables!');
+}
+if (!endpoint) {
+  throw new Error('No Wax Endpoint set in environment variables!');
+}
+if (!dapp) {
+  throw new Error('No dApp name (contract name) set in environment variables!');
+}
+
 export { chainId, endpoint, dapp };
