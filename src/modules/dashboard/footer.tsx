06c70bac713954696605e1ba36e51bc08ee30a9c
TheBoringDude <iamcoderx@gmail.com>
2021-12-05 23:31:43 +0800

    update footer

--- src/modules/dashboard/footer.tsx
+++ src/modules/dashboard/footer.tsx
@@ -1,17 +1,29 @@
 const DashFooter = () => {
   return (
-    <footer className="text-center py-8">
-      <p className="tracking-wide text-white text-sm">
-        &copy; {new Date().getFullYear()} | shomaii dApp -{' '}
-        <a
-          className="hover:underline"
-          href="https://www.worldofcryptopups.cf/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          World of Cryptopups
-        </a>
-      </p>
+    <footer className="text-center py-8 bg-gunmetal">
+      <div className="mx-auto w-11/12 text-white">
+        <div className="w-full flex items-center justify-between">
+          <div className="text-left">
+            <h4 className="text-xl font-black mb-1 text-deep-champagne">shomai dApp</h4>
+            <p className="text-sm tracking-wide">&copy; 2021 - All Rights Reserved</p>
+          </div>
+
+          <div>
+            <ul>
+              <li className="my-1">
+                <a
+                  className="hover:underline"
+                  href="https://www.worldofcryptopups.cf/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  World of Cryptopups
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
